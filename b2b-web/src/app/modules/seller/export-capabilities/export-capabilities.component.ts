19ff9c48ef0a66395c8f87de5f8e115b415299c2
tanu <tanu.kumari@scorpius.com.my>
2022-03-15 16:37:15 +0530

    r & d form done

---
+++ b2b-web/src/app/modules/seller/export-capabilities/export-capabilities.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-export-capabilities',
+  templateUrl: './export-capabilities.component.html',
+  styleUrls: ['./export-capabilities.component.scss']
+})
+export class ExportCapabilitiesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- b2b-web/src/app/modules/seller/export-capabilities/export-capabilities.module.ts
+++ b2b-web/src/app/modules/seller/export-capabilities/export-capabilities.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ExportCapabilitiesRoutingModule } from './export-capabilities-routing.module';
+import { ExportCapabilitiesComponent } from './export-capabilities.component';
+import { ManageComponent } from './manage/manage.component';
+import { ListComponent } from './list/list.component';
+import { DetailsComponent } from './details/details.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ExportCapabilitiesComponent,
+    ManageComponent,
+    ListComponent,
+    DetailsComponent
+  ],
   imports: [
     CommonModule,
     ExportCapabilitiesRoutingModule
