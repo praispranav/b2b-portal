19a26bacf558d689c25d9b6b0c5f1253190a2f29
Naved Ali <Naved.ali.scorpius.com.my>
2022-04-18 22:41:00 +0530

    Error Resolved

--- src/app/modules/seller/company/components/form-company-profile/form-company-profile.component.ts
+++ src/app/modules/seller/company/components/form-company-profile/form-company-profile.component.ts
@@ -2,9 +2,9 @@ import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-form-company-profile',
-  templateUrl: './form-company-profile.component.html',
-  styleUrls: ['./form-company-profile.component.scss']
+  selector: "app-form-company-profile",
+  templateUrl: "./form-company-profile.component.html",
+  styleUrls: ["./form-company-profile.component.scss"],
 })
 export class FormCompanyProfileComponent implements OnInit {
   forgotPasswordForm: FormGroup;
