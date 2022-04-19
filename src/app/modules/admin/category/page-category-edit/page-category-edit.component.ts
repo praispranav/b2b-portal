a3ce03dd84d593d56496964837e3d05f9ab6a53b
tanu <tanu.kumari@scorpius.com.my>
2022-04-19 21:14:45 +0530

    page-category-edit-done

--- src/app/modules/admin/category/page-category-edit/page-category-edit.component.ts
+++ src/app/modules/admin/category/page-category-edit/page-category-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-page-category-edit',
@@ -7,8 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PageCategoryEditComponent implements OnInit {
-  constructor() { }
+  categoryForm: FormGroup;
+  keywords = ["Smith", "Jane"];
+  handlePreview;
 
-  ngOnInit(): void {
+  options = [
+    { value: "jack", label: "Jacks" },
+    { value: "lucy", label: "Lucy" },
+    { value: "disabled", label: "Disabled", disabled: true },
+  ];
+  fileList = [
+    {
+      uid: -1,
+      name: 'xxx.png',
+      status: 'done',
+      url: 'http://pages.revox.io/dashboard/3.0.0/html/condensed/assets/img/profiles/avatar_small2x.jpg'
+    }
+  ];
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  get f() {
+    return this.categoryForm.controls;
+  }
+
+  ngOnInit() {
+    this.buildCategoryForm();
+  }
+
+  buildCategoryForm() {
+    this.categoryForm = this.formBuilder.group({
+      name: ["", [Validators.required]],
+      option: ["", [Validators.required]],
+      title: ["", [Validators.required]],
+      description: ["", [Validators.required]],
+      keywords: ["", [Validators.required]],
+    });
+  }
+
+  subCategoryForm() {
+    const formData = this.categoryForm.value;
+    // API CALL
+    console.log(formData);
   }
 }
