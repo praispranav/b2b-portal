42385dbf2bef26f9661219b102511a5f8ef5a5bf
Naved Ali <Naved.ali.scorpius.com.my>
2022-04-26 22:41:23 +0530

    Code Added

--- src/app/modules/seller/product/product.module.ts
+++ src/app/modules/seller/product/product.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { ReactiveFormsModule } from "@angular/forms";
 import { ProductRoutingModule } from "./product-routing.module";
+import { pgSelectModule } from '../../../@pages/components/select/select.module';
+import { FormProductDetailsComponent } from './components/form-product-details/form-product-details.component';
+import { FormProductInformationComponent } from './components/form-product-information/form-product-information.component';
 import { PageProductAddComponent } from "./page-product-add/page-product-add.component";
 import { PageProductEditComponent } from "./page-product-edit/page-product-edit.component";
 import { PageProductListComponent } from "./page-product-list/page-product-list.component";
@@ -10,6 +13,8 @@ import { PageProductViewComponent } from "./page-product-view/page-product-view.
 
 @NgModule({
   declarations: [
+    FormProductDetailsComponent,
+    FormProductInformationComponent,
     PageProductAddComponent,
     PageProductEditComponent,
     PageProductListComponent,
@@ -20,6 +25,7 @@ import { PageProductViewComponent } from "./page-product-view/page-product-view.
     FormsModule,
     ReactiveFormsModule,
     ProductRoutingModule,
+    pgSelectModule
   ],
 })
 export class ProductModule {}
