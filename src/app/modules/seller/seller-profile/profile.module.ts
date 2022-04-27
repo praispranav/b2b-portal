a0f6f5514ceb19f2b44452e3336b116e6329767b
sriti04 <ritishri@scorpius.com.my>
2022-04-27 16:03:59 +0530

    form e and d changes

--- src/app/modules/seller/seller-profile/profile.module.ts
+++ src/app/modules/seller/seller-profile/profile.module.ts
@@ -7,27 +7,22 @@ import { CertificateCenterComponent } from './components/certificate-center/cert
 import { FormCompanyDetailComponent } from "./components/form-company-detail/form-company-detail.component";
 import { FormCompanyProfileComponent } from "./components/form-company-profile/form-company-profile.component";
 import { FormQualityControlComponent } from './components/form-quality-control/form-quality-control.component';
-import { RAndDFormComponent } from './components/r-and-d-form/r-and-d-form.component';
+import { FormRAndDComponent } from './components/form-r-and-d/form-r-and-d.component';
 import { PageProfileAddComponent } from "./page-profile-add/page-profile-add.component";
 import { PageProfileEditComponent } from "./page-profile-edit/page-profile-edit.component";
 import { PageProfileViewComponent } from "./page-profile-view/page-profile-view.component";
-// import { FormQualityControlComponent } from './components/form-quality-control/form-quality-control.component';
-import { FormRAndDComponent } from './components/form-r-and-d/form-r-and-d.component';
-// import { CertificateCenterComponent } from './components/certificate-center/certificate-center.component';
-// import { FormCompnyDetailComponent } from './components/form-compny-detail/form-compny-detail.component';
-
 
 @NgModule({
   declarations: [
+    CertificateCenterComponent,
     FormCompanyDetailComponent,
     FormCompanyProfileComponent,
+    FormQualityControlComponent,
+    FormRAndDComponent,
     PageProfileAddComponent,
     PageProfileEditComponent,
     PageProfileViewComponent,
-    FormQualityControlComponent,
-    CertificateCenterComponent,
-    // FormCompnyDetailComponent,
-    FormRAndDComponent,
+
   ],
   imports: [
     CommonModule,
