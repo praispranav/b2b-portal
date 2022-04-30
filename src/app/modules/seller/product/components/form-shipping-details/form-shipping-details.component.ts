b834409893e38d5ea46b85400d8c1b67802580db
Naved Ali <Naved.ali.scorpius.com.my>
2022-04-30 22:18:33 +0530

    Code fixes

--- src/app/modules/seller/product/components/form-shipping-details/form-shipping-details.component.ts
+++ src/app/modules/seller/product/components/form-shipping-details/form-shipping-details.component.ts
@@ -1,23 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-form-shipping-details',
-  templateUrl: './form-shipping-details.component.html',
-  styleUrls: ['./form-shipping-details.component.scss']
+  selector: "app-form-shipping-details",
+  templateUrl: "./form-shipping-details.component.html",
+  styleUrls: ["./form-shipping-details.component.scss"],
 })
 export class FormShippingDetailsComponent implements OnInit {
-
   shippingDetailsForm: FormGroup;
 
-  shippingModeList: any[] =
-    [
-      { mode: "Air Transport" },
-      { mode: "Express Delivery" },
-      { mode: "Land Transportation" },
-      { mode: "Ocen Shipping" },
-    ]
-  constructor(private formBuilder: FormBuilder) { }
+  shippingModeList: any[] = [
+    { mode: "Air Transport" },
+    { mode: "Express Delivery" },
+    { mode: "Land Transportation" },
+    { mode: "Ocen Shipping" },
+  ];
+  constructor(private formBuilder: FormBuilder) {}
 
   get f() {
     return this.shippingDetailsForm.controls;
@@ -28,12 +26,11 @@ export class FormShippingDetailsComponent implements OnInit {
   }
   buildProductDetails() {
     this.shippingDetailsForm = this.formBuilder.group({
-      quantity: ['', [Validators.required]],
-      estLoadTime: ['', [Validators.required]],
-      shippingPort: ['', [Validators.required]],
-      packagingDescription: ['', [Validators.required]],
-      shippingMode: ['', [Validators.required]],
+      quantity: ["", [Validators.required]],
+      estLoadTime: ["", [Validators.required]],
+      shippingPort: ["", [Validators.required]],
+      packagingDescription: ["", [Validators.required]],
+      shippingMode: ["", [Validators.required]],
     });
   }
-
 }
