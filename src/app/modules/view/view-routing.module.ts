196c93af49e5af1ea235b49bf99a00b63cd95edd
Naved Ali <Naved.ali.scorpius.com.my>
2022-05-04 17:30:56 +0530

    Structure Chnages

--- src/app/modules/view/view-routing.module.ts
+++ src/app/modules/view/view-routing.module.ts
@@ -1,55 +1,71 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PageAboutUsComponent } from './page-about-us/page-about-us.component';
-import { PageContactUsComponent } from './page-contact-us/page-contact-us.component';
-import { PageError404Component } from './page-error404/page-error404.component';
-import { PageError500Component } from './page-error500/page-error500.component';
-import { PagePartnerComponent } from './page-partner/page-partner.component';
-import { PageSupportComponent } from './page-support/page-support.component';
-import { PageTermsAndConditionComponent } from './page-terms-and-condition/page-terms-and-condition.component';
-import { PageTermsOfUseComponent } from './page-terms-of-use/page-terms-of-use.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PageAboutUsComponent } from "./page-about-us/page-about-us.component";
+import { PageContactUsComponent } from "./page-contact-us/page-contact-us.component";
+import { PageError404Component } from "./page-error404/page-error404.component";
+import { PageError500Component } from "./page-error500/page-error500.component";
+import { PageInspectionServiceComponent } from "./page-inspection-service/page-inspection-service.component";
+import { PageLogisticSolutionsComponent } from "./page-logistic-solutions/page-logistic-solutions.component";
+import { PagePartnerComponent } from "./page-partner/page-partner.component";
+import { PageSupplierBackgroundSearchComponent } from "./page-supplier-background-search/page-supplier-background-search.component";
+import { PageSupplierReportComponent } from "./page-supplier-report/page-supplier-report.component";
+import { PageSupportComponent } from "./page-support/page-support.component";
+import { PageTermsAndConditionComponent } from "./page-terms-and-condition/page-terms-and-condition.component";
+import { PageTermsOfUseComponent } from "./page-terms-of-use/page-terms-of-use.component";
 
 const routes: Routes = [
   {
-    path: 'about-us',
+    path: "about-us",
     component: PageAboutUsComponent,
   },
   {
-    path: 'contact-us',
+    path: "contact-us",
     component: PageError500Component,
   },
   {
-    path: 'error-404',
+    path: "error-404",
     component: PageError500Component,
   },
   {
-    path: 'error-500',
+    path: "error-500",
     component: PageError500Component,
   },
   {
-    path: 'partner',
+    path: "inspection-service",
+    component: PageInspectionServiceComponent,
+  },
+  {
+    path: "logistic-solutions",
+    component: PageLogisticSolutionsComponent,
+  },
+  {
+    path: "partner",
     component: PagePartnerComponent,
   },
   {
-    path: 'support',
+    path: "supplier-background-search",
+    component: PageSupplierBackgroundSearchComponent,
+  },
+  {
+    path: "supplier-report",
+    component: PageSupplierReportComponent,
+  },
+  {
+    path: "support",
     component: PageSupportComponent,
   },
   {
-    path: 'terms-and-condition',
+    path: "terms-and-condition",
     component: PageTermsAndConditionComponent,
   },
   {
-    path: 'terms-of-use',
+    path: "terms-of-use",
     component: PageTermsOfUseComponent,
   },
   {
-    path: "trade-services",
-    loadChildren: "./trade-services/trade-services.module#TradeServicesModule",
+    path: "**",
+    redirectTo: "error-404",
   },
-  {
-    path: '**',
-    redirectTo: 'error-404',
-  }
 ];
 
 @NgModule({
--- src/app/modules/view/view.module.ts
+++ src/app/modules/view/view.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ViewRoutingModule } from './view-routing.module';
+import { pgCardModule } from "../../@pages/components/card/card.module";
+import { SharedModule } from "../../@pages/components/shared.module";
+
 import { PageAboutUsComponent } from './page-about-us/page-about-us.component';
 import { PageContactUsComponent } from './page-contact-us/page-contact-us.component';
 import { PageError404Component } from './page-error404/page-error404.component';
 import { PageError500Component } from './page-error500/page-error500.component';
+import { PageInspectionServiceComponent } from "./page-inspection-service/page-inspection-service.component";
+import { PageLogisticSolutionsComponent } from "./page-logistic-solutions/page-logistic-solutions.component";
 import { PagePartnerComponent } from './page-partner/page-partner.component';
+import { PageSupplierBackgroundSearchComponent } from "./page-supplier-background-search/page-supplier-background-search.component";
+import { PageSupplierReportComponent } from "./page-supplier-report/page-supplier-report.component";
 import { PageSupportComponent } from './page-support/page-support.component';
 import { PageTermsAndConditionComponent } from './page-terms-and-condition/page-terms-and-condition.component';
 import { PageTermsOfUseComponent } from './page-terms-of-use/page-terms-of-use.component';
@@ -17,14 +23,20 @@ import { PageTermsOfUseComponent } from './page-terms-of-use/page-terms-of-use.c
     PageContactUsComponent,
     PageError404Component,
     PageError500Component,
+    PageInspectionServiceComponent,
+    PageLogisticSolutionsComponent,
     PagePartnerComponent,
+    PageSupplierBackgroundSearchComponent,
+    PageSupplierReportComponent,
     PageSupportComponent,
     PageTermsAndConditionComponent,
     PageTermsOfUseComponent
   ],
   imports: [
     CommonModule,
-    ViewRoutingModule
+    ViewRoutingModule,
+    pgCardModule,
+    SharedModule,
   ]
 })
 export class ViewModule { }
