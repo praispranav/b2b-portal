22c7a5394839b4ece10c3d86c762038550b5c0c1
Naved Ali <Naved.ali.scorpius.com.my>
2022-05-06 00:52:28 +0530

    Chnages Added

--- src/app/modules/user-auth/page-sign-up/page-sign-up.component.ts
+++ src/app/modules/user-auth/page-sign-up/page-sign-up.component.ts
@@ -6,6 +6,17 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./page-sign-up.component.scss"],
 })
 export class PageSignUpComponent implements OnInit {
+  txtCname;
+  txtfname;
+  txtsname;
+  txtemail;
+  txtpassword;
+  txtcpassword;
+  txtCountry;
+  txtaddress1;
+  txtaddress2;
+  txtmNumber;
+
   constructor() {}
 
   ngOnInit(): void {}
