9873bda8896901df5e67d9d65773d4925bc497e2
Naved Ali <Naved.ali.scorpius.com.my>
2022-05-07 23:19:42 +0530

    Page Added

--- src/app/modules/view/view-routing.module.ts
+++ src/app/modules/view/view-routing.module.ts
@@ -1,71 +1,86 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { PageAboutUsComponent } from './page-about-us/page-about-us.component';
-import { PageContactUsComponent } from './page-contact-us/page-contact-us.component';
-import { PageError404Component } from './page-error404/page-error404.component';
-import { PageError500Component } from './page-error500/page-error500.component';
-import { PagePartnerComponent } from './page-partner/page-partner.component';
-import { PageSellerCatalogueHomeComponent } from './page-seller-catalogue-home/page-seller-catalogue-home.component';
-import { PageSellerCatalogueProductComponent } from './page-seller-catalogue-product/page-seller-catalogue-product.component';
-import { PageSellerCatalogueProfileComponent } from './page-seller-catalogue-profile/page-seller-catalogue-profile.component';
-import { PageSellerCatalogueContactComponent } from './page-seller-catalogue-contact/page-seller-catalogue-contact.component';
-import { PageSupportComponent } from './page-support/page-support.component';
-import { PageTermsAndConditionComponent } from './page-terms-and-condition/page-terms-and-condition.component';
-import { PageTermsOfUseComponent } from './page-terms-of-use/page-terms-of-use.component';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PageAboutUsComponent } from "./page-about-us/page-about-us.component";
+import { PageContactUsComponent } from "./page-contact-us/page-contact-us.component";
+import { PageError404Component } from "./page-error404/page-error404.component";
+import { PageError500Component } from "./page-error500/page-error500.component";
+import { PageHomeComponent } from "./page-home/page-home.component";
+import { PagePartnerComponent } from "./page-partner/page-partner.component";
+import { PageProductListComponent } from "./page-product-list/page-product-list.component";
+import { PageProductViewComponent } from "./page-product-view/page-product-view.component";
+import { PageSellerCatalogueHomeComponent } from "./page-seller-catalogue-home/page-seller-catalogue-home.component";
+import { PageSellerCatalogueProductComponent } from "./page-seller-catalogue-product/page-seller-catalogue-product.component";
+import { PageSellerCatalogueProfileComponent } from "./page-seller-catalogue-profile/page-seller-catalogue-profile.component";
+import { PageSellerCatalogueContactComponent } from "./page-seller-catalogue-contact/page-seller-catalogue-contact.component";
+import { PageSupportComponent } from "./page-support/page-support.component";
+import { PageTermsAndConditionComponent } from "./page-terms-and-condition/page-terms-and-condition.component";
+import { PageTermsOfUseComponent } from "./page-terms-of-use/page-terms-of-use.component";
 
 const routes: Routes = [
   {
-    path: 'about-us',
+    path: "about-us",
     component: PageAboutUsComponent,
   },
   {
-    path: 'contact-us',
+    path: "contact-us",
     component: PageError500Component,
   },
   {
-    path: 'error-404',
+    path: "error-404",
     component: PageError500Component,
   },
   {
-    path: 'error-500',
+    path: "error-500",
     component: PageError500Component,
   },
   {
-    path: 'partner',
+    path: "home",
+    component: PageHomeComponent,
+  },
+  {
+    path: "partner",
     component: PagePartnerComponent,
   },
   {
-    path: 'seller-catalogue-contact',
+    path: "product-list",
+    component: PageProductListComponent,
+  },
+  {
+    path: "product-view",
+    component: PageProductViewComponent,
+  },
+  {
+    path: "seller-catalogue-contact",
     component: PageSellerCatalogueContactComponent,
   },
   {
-    path: 'seller-catalogue-home',
+    path: "seller-catalogue-home",
     component: PageSellerCatalogueHomeComponent,
   },
   {
-    path: 'seller-catalogue-product',
+    path: "seller-catalogue-product",
     component: PageSellerCatalogueProductComponent,
   },
   {
-    path: 'seller-catalogue-profile',
+    path: "seller-catalogue-profile",
     component: PageSellerCatalogueProfileComponent,
   },
   {
-    path: 'support',
+    path: "support",
     component: PageSupportComponent,
   },
   {
-    path: 'terms-and-condition',
+    path: "terms-and-condition",
     component: PageTermsAndConditionComponent,
   },
   {
-    path: 'terms-of-use',
+    path: "terms-of-use",
     component: PageTermsOfUseComponent,
   },
   {
-    path: '**',
-    redirectTo: 'error-404',
-  }
+    path: "**",
+    redirectTo: "error-404",
+  },
 ];
 
 @NgModule({
