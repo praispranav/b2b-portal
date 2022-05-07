257c1186f329391f1fb837c19f07445db4bcf30d
Naved Ali <Naved.ali.scorpius.com.my>
2022-05-07 15:48:14 +0530

    Chnages Added

---
+++ src/app/modules/view/page-seller-catalogue-feed/page-seller-catalogue-feed.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-seller-catalogue-feed',
+  templateUrl: './page-seller-catalogue-feed.component.html',
+  styleUrls: ['./page-seller-catalogue-feed.component.scss']
+})
+export class PageSellerCatalogueFeedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/modules/view/view-routing.module.ts
+++ src/app/modules/view/view-routing.module.ts
@@ -5,6 +5,11 @@ import { PageContactUsComponent } from './page-contact-us/page-contact-us.compon
 import { PageError404Component } from './page-error404/page-error404.component';
 import { PageError500Component } from './page-error500/page-error500.component';
 import { PagePartnerComponent } from './page-partner/page-partner.component';
+import { PageSellerCatalogueHomeComponent } from './page-seller-catalogue-home/page-seller-catalogue-home.component';
+import { PageSellerCatalogueProductComponent } from './page-seller-catalogue-product/page-seller-catalogue-product.component';
+import { PageSellerCatalogueProfileComponent } from './page-seller-catalogue-profile/page-seller-catalogue-profile.component';
+import { PageSellerCatalogueContactComponent } from './page-seller-catalogue-contact/page-seller-catalogue-contact.component';
+import { PageSellerCatalogueFeedComponent } from './page-seller-catalogue-feed/page-seller-catalogue-feed.component';
 import { PageSupportComponent } from './page-support/page-support.component';
 import { PageTermsAndConditionComponent } from './page-terms-and-condition/page-terms-and-condition.component';
 import { PageTermsOfUseComponent } from './page-terms-of-use/page-terms-of-use.component';
@@ -29,6 +34,26 @@ const routes: Routes = [
   {
     path: 'partner',
     component: PagePartnerComponent,
+  },
+  {
+    path: 'seller-catalogue-contact',
+    component: PageSellerCatalogueContactComponent,
+  },
+  {
+    path: 'seller-catalogue-feed',
+    component: PageSellerCatalogueFeedComponent,
+  },
+  {
+    path: 'seller-catalogue-home',
+    component: PageSellerCatalogueHomeComponent,
+  },
+  {
+    path: 'seller-catalogue-product',
+    component: PageSellerCatalogueProductComponent,
+  },
+  {
+    path: 'seller-catalogue-profile',
+    component: PageSellerCatalogueProfileComponent,
   },
   {
     path: 'support',
