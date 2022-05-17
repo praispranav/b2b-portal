a26c657d0e3d39d89eeefa656328588a03bdb60a
Naved Ali <Naved.ali.scorpius.com.my>
2022-05-17 21:49:29 +0530

    Changes Agges

--- src/app/modules/view/page-contact-us/page-contact-us.component.ts
+++ src/app/modules/view/page-contact-us/page-contact-us.component.ts
@@ -1,72 +1,80 @@
-import { Component, OnInit,Input } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import createNumberMask from 'text-mask-addons/dist/createNumberMask';
-import { pgSelectfx } from '../../../@pages/components/cs-select/select.module';
-import * as moment from 'moment';
+import { Component, OnInit, Input } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import createNumberMask from "text-mask-addons/dist/createNumberMask";
+import { pgSelectfx } from "../../../@pages/components/cs-select/select.module";
+import * as moment from "moment";
 // import { OwlOptions } from 'ngx-owl-carousel-o';
 @Component({
-  selector: 'app-page-contact-us',
-  templateUrl: './page-contact-us.component.html',
-  styleUrls: ['./page-contact-us.component.scss']
+  selector: "app-page-contact-us",
+  templateUrl: "./page-contact-us.component.html",
+  styleUrls: ["./page-contact-us.component.scss"],
 })
-export class PageContactUsComponent implements OnInit { 
- contactUsForm:FormGroup;
- mask = {
-  date: [/[1-9]/, /\d/, '/', /\d/, /\d/, '/', /\d/, /\d/, /\d/, /\d/],
-  telephone: ['(', /[1-9]/, /\d/, /\d/, ')', ' ', /\d/, /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/],
-  custom: [/[1-9]/, /\d/, '-', /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/],
-  ssn: [/[1-9]/, /\d/, /\d/, '-', /\d/, /\d/, '-', /\d/, /\d/, /\d/, /\d/]
-};
-numberMask = createNumberMask({
-  prefix: '$ ',
-  suffix: ''
-});
-wierdMask = createNumberMask({
-  prefix: '',
-  suffix: '',
-  thousandsSeparatorSymbol: '.',
-  allowDecimal: true,
-  decimalSymbol: ','
-});
-dollarPrefix = createNumberMask({
-  prefix: '$ ',
-  suffix: '',
-  allowDecimal: true
-});
-range = createNumberMask({
-  prefix: '',
-  suffix: '',
-  integerLimit: 4
-});
+export class PageContactUsComponent implements OnInit {
+  contactUsForm: FormGroup;
+  mask = {
+    date: [/[1-9]/, /\d/, "/", /\d/, /\d/, "/", /\d/, /\d/, /\d/, /\d/],
+    telephone: [
+      "(",
+      /[1-9]/,
+      /\d/,
+      /\d/,
+      ")",
+      " ",
+      /\d/,
+      /\d/,
+      /\d/,
+      "-",
+      /\d/,
+      /\d/,
+      /\d/,
+      /\d/,
+    ],
+    custom: [/[1-9]/, /\d/, "-", /\d/, /\d/, /\d/, /\d/, /\d/, /\d/, /\d/],
+    ssn: [/[1-9]/, /\d/, /\d/, "-", /\d/, /\d/, "-", /\d/, /\d/, /\d/, /\d/],
+  };
+  numberMask = createNumberMask({
+    prefix: "$ ",
+    suffix: "",
+  });
+  wierdMask = createNumberMask({
+    prefix: "",
+    suffix: "",
+    thousandsSeparatorSymbol: ".",
+    allowDecimal: true,
+    decimalSymbol: ",",
+  });
+  dollarPrefix = createNumberMask({
+    prefix: "$ ",
+    suffix: "",
+    allowDecimal: true,
+  });
+  range = createNumberMask({
+    prefix: "",
+    suffix: "",
+    integerLimit: 4,
+  });
 
- constructor(private fb:FormBuilder) {
-  this.contactUsForm=this.fb.group(
-    {
-     firstName:[''],
-     lastName:[''],
-     companyName:[''],
-     State:[''],
-     City:[''],
-     Country:[''],
-     mobile:[''],
-     countryCode:[''],
-     email:['',[Validators.email]],
-     description:['',[Validators.required]]
+  constructor(private fb: FormBuilder) {}
 
+  get f() { return this.contactUsForm.controls; }
 
-
-    }
-  )
-  }
-
-
- 
   ngOnInit() {
-   
-  }
-  
-  getFormControl(name) {
-    return this.contactUsForm.controls[name];
+    this.buidlContactUsForm();
   }
 
+  buidlContactUsForm() {
+    this.contactUsForm = this.fb.group({
+      companyName: ["", [Validators.required]],
+      firstName: ["", [Validators.required]],
+      lastName: [""],
+
+      State: [""],
+      City: [""],
+      Country: [""],
+      mobile: [""],
+      countryCode: [""],
+      email: ["", [Validators.email]],
+      description: ["", [Validators.required]],
+    });
+  }
 }
