2a948e8866676fa6eadf7ecea8a3cce76fe56ea4
sriti04 <ritishri@scorpius.com.my>
2022-06-01 11:38:36 +0530

    component added

---
+++ src/app/modules/seller/seller-profile/components/form-seller-type/form-seller-type.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-seller-type',
+  templateUrl: './form-seller-type.component.html',
+  styleUrls: ['./form-seller-type.component.scss']
+})
+export class FormSellerTypeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
