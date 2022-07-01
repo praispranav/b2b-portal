52a4ba2f01cd6cfc445a2d1e31e734865a77ce9b
Suraj <surajk8102000@gmail.com>
2022-07-01 21:12:57 +0530

    complaint pages

--- src/app/features/help/page-complain-list/page-complain-list.component.ts
+++ src/app/features/help/page-complain-list/page-complain-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
 
 @Component({
   selector: 'app-page-complain-list',
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageComplainListComponent implements OnInit {
 
-  constructor() { }
+ 
+  constructor() {
+    
+   }
 
   ngOnInit() {
   }
+
+  
 
 }
