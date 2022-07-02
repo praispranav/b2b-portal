ef849101526d0cc7302b1bc29df06d1454a3b1a3
Sudhanshu kumar <sudhanshugolu2003@gmail.com>
2022-07-02 10:07:02 +0530

    all side bars

--- src/app/@pages/layouts/agent-layout/agent-layout.component.ts
+++ src/app/@pages/layouts/agent-layout/agent-layout.component.ts
@@ -8,7 +8,58 @@ import { RootLayout } from '../root/root.component';
   encapsulation: ViewEncapsulation.None
 })
 export class AgentLayoutComponent extends RootLayout implements OnInit {
-  menuLinks = [];
+  menuLinks = [
+    {
+      label: "Home",
+      routerLink: "seller-dashboard",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Profile",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Assigned Account",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Verify Accounts",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Accounts Status ",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Help",
+      toggle: "close",
+      iconType: "fi",
+      iconName: "shield",
+      submenu: [
+        {
+          label: "Complaints",
+          routerLink: "buyer-dashboard",
+        },
+        {
+          label: "Disputes",
+          routerLink: "buyer-dashboard",
+        },
+        {
+          label: "Suggestions",
+          routerLink: "buyer-dashboard",
+        }
+      ],
+    },
+  ];
   ngOnInit() {
     this.changeLayout('menu-pin');
     //Will sidebar close on screens below 1024
--- src/app/@pages/layouts/associate-layout/associate-layout.component.ts
+++ src/app/@pages/layouts/associate-layout/associate-layout.component.ts
@@ -8,7 +8,70 @@ import { RootLayout } from '../root/root.component';
   encapsulation: ViewEncapsulation.None
 })
 export class AssociateLayoutComponent extends RootLayout implements OnInit {
-  menuLinks = [];
+  menuLinks = [
+    {
+      label: "Home",
+      routerLink: "seller-dashboard",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Profile",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Send Request",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Add into Protfolio",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "List Seller",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Request for Seller Verification",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Seller Verification Status",
+      routerLink: "seller-profile",
+      iconType: "fi",
+      iconName: "shield",
+    },
+    {
+      label: "Help",
+      toggle: "close",
+      iconType: "fi",
+      iconName: "shield",
+      submenu: [
+        {
+          label: "Complaints",
+          routerLink: "buyer-dashboard",
+        },
+        {
+          label: "Disputes",
+          routerLink: "buyer-dashboard",
+        },
+        {
+          label: "Suggestions",
+          routerLink: "buyer-dashboard",
+        }
+      ],
+    },
+  ];
   ngOnInit() {
     this.changeLayout('menu-pin');
     //Will sidebar close on screens below 1024
