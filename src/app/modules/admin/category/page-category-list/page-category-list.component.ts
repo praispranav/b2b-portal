4c3192f271d39ca30ce98cd030d8f04190894b95
utkarsh000sharma <49993911+utkarsh000sharma@users.noreply.github.com>
2022-07-04 05:15:21 +0530

    Chnages Added

--- src/app/modules/admin/category/page-category-list/page-category-list.component.ts
+++ src/app/modules/admin/category/page-category-list/page-category-list.component.ts
@@ -1,12 +1,64 @@
-import { Component, OnInit } from "@angular/core";
-
+import { Component, OnInit } from '@angular/core';
+import { ITreeOptions, TreeModel, TreeNode } from 'angular-tree-component';
+import { TreeOptions } from 'angular-tree-component/dist/models/tree-options.model';
+import { ProviderMaterCategoryService } from '../../../../core/providers/master/provider-mater-category.service';
 @Component({
   selector: "app-page-category-list",
   templateUrl: "./page-category-list.component.html",
   styleUrls: ["./page-category-list.component.scss"],
 })
 export class PageCategoryListComponent implements OnInit {
-  constructor() {}
+  masterCategoryParentLevelList: any[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private providerMaterCategoryService: ProviderMaterCategoryService
+  ) { }
+
+  ngOnInit() {
+    this.getMaterCategoryListByFilter(0, 1000, {});
+  }
+
+  getMaterCategoryListByFilter(index: number, length: number, query: any = {}) {
+    this.providerMaterCategoryService
+      .getMaterCategoryListByFilter(index, length, query)
+      .subscribe((res) => {
+        this.masterCategoryParentLevelList = res.data.map(i => {
+          return {
+            ...i, children: [
+              { name: 'Loading...' }
+            ]
+          }
+        });
+      });
+  }
+
+  filterFn(value: string, treeModel: TreeModel) {
+    treeModel.filterNodes((node: TreeNode) => this.fuzzysearch(value, node.data.name));
+  }
+
+  fuzzysearch(needle: string, haystack: string) {
+    const haystackLC = haystack.toLowerCase();
+    const needleLC = needle.toLowerCase();
+
+    const hlen = haystack.length;
+    const nlen = needleLC.length;
+
+    if (nlen > hlen) {
+      return false;
+    }
+    if (nlen === hlen) {
+      return needleLC === haystackLC;
+    }
+    outer: for (let i = 0, j = 0; i < nlen; i++) {
+      const nch = needleLC.charCodeAt(i);
+
+      while (j < hlen) {
+        if (haystackLC.charCodeAt(j++) === nch) {
+          continue outer;
+        }
+      }
+      return false;
+    }
+    return true;
+  }
 }
