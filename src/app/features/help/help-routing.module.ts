56cb2f87d8a82b7ab4fe09665a2167a9c06b9b39
utkarsh000sharma <49993911+utkarsh000sharma@users.noreply.github.com>
2022-07-12 19:38:58 +0530

    Chnages Added

--- src/app/features/help/help-routing.module.ts
+++ src/app/features/help/help-routing.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PageComplainAddComponent } from "./page-complain-add/page-complain-add.component";
 import { PageComplainListComponent } from "./page-complain-list/page-complain-list.component";
-import { PageComplainViewComponent } from "./page-complain-view/page-complain-view.component";
 import { PageDisputeAddComponent } from "./page-dispute-add/page-dispute-add.component";
 import { PageDisputeListComponent } from "./page-dispute-list/page-dispute-list.component";
-import { PageDisputeViewComponent } from "./page-dispute-view/page-dispute-view.component";
 import { PageSuggestionAddComponent } from "./page-suggestion-add/page-suggestion-add.component";
 import { PageSuggestionListComponent } from "./page-suggestion-list/page-suggestion-list.component";
-import { PageSuggestionViewComponent } from "./page-suggestion-view/page-suggestion-view.component";
 
 
 const routes: Routes = [
@@ -26,10 +23,6 @@ const routes: Routes = [
     component: PageComplainListComponent
   },
   {
-    path: "complain-view",
-    component: PageComplainViewComponent
-  },
-  {
     path: "dispute-add",
     component: PageDisputeAddComponent
   },
@@ -38,21 +31,13 @@ const routes: Routes = [
     component: PageDisputeListComponent
   },
   {
-    path: "dispute-view",
-    component: PageDisputeViewComponent
-  },
-  {
     path: "suggestion-add",
     component: PageSuggestionAddComponent
   },
   {
     path: "suggestion-list",
     component: PageSuggestionListComponent
-  },
-  {
-    path: "suggestion-view",
-    component: PageSuggestionViewComponent
-  },
+  }
 ];
 
 @NgModule({
--- src/app/features/help/page-dispute-add/page-dispute-add.component.ts
+++ src/app/features/help/page-dispute-add/page-dispute-add.component.ts
@@ -1,50 +1,48 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ProviderHelpDisputeService } from '../../../core/providers/user/provider-help-dispute.service';
+
 @Component({
   selector: 'app-page-dispute-add',
   templateUrl: './page-dispute-add.component.html',
   styleUrls: ['./page-dispute-add.component.scss']
 })
 export class PageDisputeAddComponent implements OnInit {
-  complaintdispute: FormGroup
-  fileName: any;
-  checkRadioValue: boolean;
-  constructor(private formbuilder: FormBuilder, private providerHelpDisputeService: ProviderHelpDisputeService) { }
+  disputeForm: FormGroup;
 
-  get f() { return this.complaintdispute.controls }
+  constructor(
+    private formBuilder: FormBuilder,
+    private providerHelpDisputeService: ProviderHelpDisputeService
+  ) { }
+
+  get f() {
+    return this.disputeForm.controls;
+  }
 
   ngOnInit() {
-    this.buildcomplaintdispute()
+    this.buildDisputeForm();
   }
 
-  buildcomplaintdispute() {
-    this.complaintdispute = this.formbuilder.group({
-      subject: ['', [Validators.required, Validators.maxLength(200)]],
-      pageLink: ['', [Validators.required]],
-      description: ['', [Validators.required]],
+  buildDisputeForm() {
+    this.disputeForm = this.formBuilder.group({
       type: ['', [Validators.required]],
-      file: [File, [Validators.required]]
-
-    })
+      subject: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+      link: ['', [Validators.required]],
+      file: [''],
+    });
   }
 
-  async complaintdisputeHandler() {
+  async subCategoryForm() {
+    if (this.disputeForm.invalid) {
+      this.markFormGroupTouched(this.disputeForm);
+      return;
+    }
 
-    // if (this.complaintdispute.invalid) {
-    //   this.markFormGroupTouched(this.complaintdispute);
-    //   return;
-    // }
-
-    let formData = this.complaintdispute.value;
-    console.log(formData);
-
-    this.providerHelpDisputeService.addHelpDispute(this.complaintdispute.value).subscribe(
+    this.providerHelpDisputeService.addHelpDispute(this.disputeForm.value).subscribe(
       (res) => {
-        // this.resetFormGroup(formData);
+        this.resetFormGroup(this.disputeForm);
         window.alert('API Success');
-        this.complaintdispute.reset();
-        this.checkRadioValue = false;
       },
       (err) => {
         window.alert('API Error');
@@ -52,25 +50,24 @@ export class PageDisputeAddComponent implements OnInit {
     );
   }
 
-  // private markFormGroupTouched(form: FormGroup) {
-  //   Object.values(form.controls).forEach((control) => {
-  //     control.markAsTouched();
-  //     if ((control as any).controls) {
-  //       this.markFormGroupTouched(control as FormGroup);
-  //     }
-  //   });
-  // }
-
-  checkRadio(event: any) {
-    let radioValue = event.target.value;
-    console.log(radioValue);
-    this.complaintdispute.controls.type.setValue(radioValue);
+  private markFormGroupTouched(form: FormGroup) {
+    Object.values(form.controls).forEach((control) => {
+      control.markAsTouched();
+      if ((control as any).controls) {
+        this.markFormGroupTouched(control as FormGroup);
+      }
+    });
   }
-  openFile(fileInputDoc) {
-    fileInputDoc.click();
 
+  private resetFormGroup(form: FormGroup) {
+    form.reset();
   }
-  onSelectFile(event: object) {
+
+  fileUpload(files){
+    console.log(files.target.files)
+  }
+
+  clickOnInputFile(el) {
+    el.click();
   }
 }
-
