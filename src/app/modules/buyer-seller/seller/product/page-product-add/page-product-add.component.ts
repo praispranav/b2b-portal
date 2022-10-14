56a2a6de64ab25f28683f0df0cf239d826c55688
toshutech <tushar@scorpius.com.my>
2022-10-14 16:36:10 +0530

    workdone

--- src/app/modules/buyer-seller/seller/product/page-product-add/page-product-add.component.ts
+++ src/app/modules/buyer-seller/seller/product/page-product-add/page-product-add.component.ts
@@ -1,13 +1,11 @@
-import { subProductDetails } from './../components/form-product-details/form-product-details.component';
+
 import { Router } from '@angular/router';
-
-
 import { productInfo } from './../components/form-product-information/form-product-information.component';
 import { Component, OnInit } from '@angular/core';
 import { subProductDetails } from '../components/form-product-details/form-product-details.component';
 import { tradeInfo } from '../components/form-trade-information/form-trade-information.component';
 import { shippingInfo } from '../components/form-shipping-details/form-shipping-details.component';
-import { AppMessageService } from '../../../../../../core/services/app-message.service';
+
 import { ProviderProductInformationService } from '../../../../../core/providers/user/provider-product-information.service';
 
 @Component({
@@ -19,7 +17,7 @@ export class PageProductAddComponent implements OnInit {
   isAddingProductWhichNotOnPortal:boolean = false;
   
   
-  constructor( private appMessageService:AppMessageService, private router:Router,private providerProductInformationService: ProviderProductInformationService) { }
+  constructor(  private router:Router,private providerProductInformationService: ProviderProductInformationService) { }
 
 
   productInfoForm(productInfo:productInfo){
