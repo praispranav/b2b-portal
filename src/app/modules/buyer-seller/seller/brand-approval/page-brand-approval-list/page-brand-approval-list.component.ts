9876cc9177b9faeec2a3e5bb159a163ab10658b5
tanu <tanu.kumari@scorpius.com.my>
2022-10-19 16:42:28 +0530

    brand approval done

--- src/app/modules/buyer-seller/seller/brand-approval/page-brand-approval-list/page-brand-approval-list.component.ts
+++ src/app/modules/buyer-seller/seller/brand-approval/page-brand-approval-list/page-brand-approval-list.component.ts
@@ -11,11 +11,38 @@ export class PageBrandApprovalListComponent implements OnInit {
 
   constructor(
     private providerBrandApprovalService: ProviderBrandApprovalService,
-  ) { }
+  ) {
+
+    window.onresize = () => {
+      this.scrollBarHorizontal = window.innerWidth < 960;
+      this.columnModeSetting = window.innerWidth < 960 ? 'standard' : 'force';
+      this.columnModeSettingSmall = window.innerWidth < 560 ? 'standard' : 'force';
+    };
+  }
+  sortName = null;
+  sortValue = null;
+  selected = [];
+  // No Option YET
+  // https://github.com/swimlane/ngx-datatable/issues/423
+  scrollBarHorizontal = window.innerWidth < 960;
+  columnModeSetting = window.innerWidth < 960 ? 'standard' : 'force';
+  columnModeSettingSmall = window.innerWidth < 560 ? 'standard' : 'force';
+
+  // fetch(cb) {
+  //   const req = new XMLHttpRequest();
+  //   req.open('GET', `assets/data/table.json`);
+
+  //   req.onload = () => {
+  //     cb(JSON.parse(req.response));
+  //   };
+
+  //   req.send();
+  // }
 
   ngOnInit() {
     this.getBrandApprovalListByFilter(0, 1000, {});
   }
+  onActivate(event) { }
 
   getBrandApprovalListByFilter(index: number, length: number, query: any = {}) {
     this.providerBrandApprovalService
