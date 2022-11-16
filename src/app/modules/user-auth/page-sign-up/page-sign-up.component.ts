d4ea73d16ad132065fb5b1574674773d2dc2699b
shree42N <riti91557@gmail.com>
2022-11-16 13:11:26 +0530

    changes in auth

--- src/app/modules/user-auth/page-sign-up/page-sign-up.component.ts
+++ src/app/modules/user-auth/page-sign-up/page-sign-up.component.ts
@@ -4,6 +4,9 @@ import { ProviderUserAuthService } from "./../../../core/providers/auth/provider
 import { Router } from '@angular/router';
 import { MessageService } from "../../../@pages/components/message/message.service";
 import { AppMessageService } from "../../../core/services/app-message.service";
+import { ProviderMaterCountryService } from "../../../core/providers/master/provider-mater-country.service";
+import { ProviderMaterStateService } from "../../../core/providers/master/provider-mater-state.service";
+import { ProviderMaterLocationService } from "../../../core/providers/master/provider-mater-location.service";
 
 
 @Component({
@@ -14,22 +17,30 @@ import { AppMessageService } from "../../../core/services/app-message.service";
 export class PageSignUpComponent implements OnInit {
   signUpForm: FormGroup;
   selectedOption;
-  options = [
-    { value: 'India', label: 'India' },
-    { value: 'Malaysia', label: 'Malaysia' },
-    { value: 'Pakistan', label: 'Pakistan' }
-  ];
+  // options = [
+  //   { value: 'India', label: 'India' },
+  //   { value: 'Malaysia', label: 'Malaysia' },
+  //   { value: 'Pakistan', label: 'Pakistan' }
+  // ];
   codes = [
     { value: '+91', label: '+91' },
     { value: '+65', label: '+65' },
     { value: '+11', label: '+11' }
   ];
+
+
+  countries: any[] = [];
+  states: any[] = [];
+  cities: any[] = [];
   constructor(
     private router: Router,
     private formBuilder: FormBuilder,
     private providerUserAuthService: ProviderUserAuthService,
     private messageService: MessageService,
     private appMessageService: AppMessageService,
+    private providerMaterCountryService: ProviderMaterCountryService,
+    private providerMaterStateService: ProviderMaterStateService,
+    private providerMaterLocationService: ProviderMaterLocationService
   ) { }
 
   get f() {
@@ -38,6 +49,57 @@ export class PageSignUpComponent implements OnInit {
 
   ngOnInit() {
     this.buildSignUpForm();
+    this.getCountryList();
+  }
+
+  
+getCountryList(){
+  this.providerMaterCountryService.getMaterCountryList().subscribe(
+    (res: any) => {
+
+      this.countries = res.data;
+      console.log('country', this.countries)
+    },
+    (err) => {
+      console.log(err)
+    }
+  );
+}
+  onCountrySelected(e) {
+    console.log("" + e.target.value);
+    this.f.regCountry.setValue(e.target.value);
+    this.f.facCountry.setValue(e.target.value);
+  
+    this.providerMaterStateService.getMaterStateListAll(e.target.value).subscribe(
+      (res: any) => {
+        this.states = res.data[0].states;
+        console.log('state', this.states)
+      },
+      (err) => {
+        console.log(err)
+      }
+    );
+  }
+
+  onStateSelected(e) {
+    console.log("" + e.target.value);
+    this.f.regState.setValue(e.target.value);
+
+    this.f.facState.setValue(e.target.value);
+    this.providerMaterLocationService.getMaterLocationListAll(e.target.value).subscribe(
+      (res: any) => {
+        this.cities = res.data[0].cities;
+        console.log('cities', this.cities)
+      },
+      (err) => {
+        console.log(err)
+      }
+    );
+  }
+
+  onCitySelected(e) {
+    console.log("" + e.target.value);
+    this.f.regCity.setValue(e.target.value);
   }
 
   buildSignUpForm() {
@@ -54,7 +116,7 @@ export class PageSignUpComponent implements OnInit {
       address1: ["", [Validators.required]],
       city: [""],
       role: ["", [Validators.required]],
-      checkAggriment:[true]
+      checkAggriment:["", [Validators.required]]
     });
   }
 
