8ee25fc4aed632feaa94703a7159c8b28f43a021
toshutech <tushar@scorpius.com.my>
2022-12-14 14:34:03 +0530

    work done by tushar and header and footer

--- src/app/modules/view/components/footer2/footer2.component.ts
+++ src/app/modules/view/components/footer2/footer2.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Footer2Component implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+  // terms-and-condition
+  navigateToTerms() {
+    const url= this.router.serializeUrl(
+      this.router.createUrlTree(['/b2b/terms-of-use'])
+    )
+    window.open(url,"_blank");
+  }
+  
+  navigateToPrivacyPolicy() {
+    const url= this.router.serializeUrl(
+      this.router.createUrlTree(['/b2b/policies-n-rules'])
+    )
+    window.open(url,"_blank");
   }
 
 }
