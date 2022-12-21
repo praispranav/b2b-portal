a6a430c26df9c159dd9d6025e5c5261617df5503
Pranav Kumar <praispranav@gmail.com>
2022-12-21 10:28:15 +0530

    Added Product Map

--- src/app/modules/buyer-seller/seller/product/components/search-select-product-type/search-select-product-type.component.ts
+++ src/app/modules/buyer-seller/seller/product/components/search-select-product-type/search-select-product-type.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProviderCategoryService } from '../../../../../../core/providers/user/provider-category.service';
 
 @Component({
   selector: 'app-search-select-product-type',
@@ -6,10 +7,148 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-select-product-type.component.scss']
 })
 export class SearchSelectProductTypeComponent implements OnInit {
+  keyword: string = '';
+  recentCategory: any[] = [];
 
-  constructor() { }
+  searchedCategories: any[] = [];
+
+  allCategoryListType3: any = [];
+  categoryListType3:any [] = [];
+  preViewItem: any = null;
+  constructor(private categoryService: ProviderCategoryService) { }
 
   ngOnInit() {
+    this.getCategoryListType3(0, 1000, { level: "0" });
+    this.getRecentCategories();
+  }
+
+  getRecentCategories(){
+    this.categoryService.getRecent().subscribe((res)=>{
+      this.recentCategory = res.data.searchedCategories;
+      // console.log("RecentCategory", this.recentCategory)
+    });
+  }
+
+  filterCategory() {
+    const keyword = this.keyword;
+    
+    this.categoryService
+      .filterByKeyword(keyword)
+      .subscribe((res) => {
+        const newList = [];
+        const searchedCategories = res.data.searchedCategories;
+        const parents = res.data.parents;
+
+        searchedCategories.forEach((category) => {
+          const newTree = this.includeParents(parents, category);
+          newList.push({
+            mainCategoryName: category.name,
+            value: category._id,
+            label: newTree + " >> " + category.name,
+          });
+        });
+        this.searchedCategories = newList;
+        console.log("Resolved");
+      });
+  }
+
+  includeParents(parent, category, newTree = "") {
+    if (category.level == "0") return newTree;
+    else {
+      const findParent = parent.find(
+        (parentCat) => parentCat._id === category.parentId
+      );
+      // console.log("FIndParent", findParent,category.parentId, parent)
+      newTree = findParent.name + " >> " + category.name;
+      console.log("NewTree", newTree);
+      return this.includeParents(parent, findParent, newTree);
+    }
+  }
+
+  getCategoryListType3(
+    index: number,
+    length: number,
+    query: any = {}
+  ) {
+    this.categoryService
+      .getCategoryListByFilter(index, length, query)
+      .subscribe((res) => {
+        this.categoryListType3 = res.data.map((i) => {
+          this.allCategoryListType3.push(i);
+          return {
+            ...i,
+            _name: i.name,
+            _hasChildren: true,
+            _isEditable: true,
+            _isDeletable: true,
+          };
+        });
+      });
+  }
+
+  getChildren(item: any, nodeEl: any) {
+    if (this.preViewItem) {
+      this.remChildren(item);
+    }
+    this.categoryService
+      .getCategoryListByFilter(0, 1000, { parentId: item["_id"] })
+      .subscribe((res) => {
+        item["_toggle"] = true;
+        this.preViewItem = item;
+        this.preViewItem["_nodeEl"] = nodeEl;
+        item["children"] = res.data.map((i) => {
+          this.allCategoryListType3.push(i);
+          return {
+            ...i,
+            _name: i.name,
+            _hasChildren: true,
+            _isEditable: true,
+            _isDeletable: true,
+          };
+        });
+      });
+  }
+
+  remChildren(cur: any) {
+    this.removeRecChild(cur, this.categoryListType3);
+    this.preViewItem = null;
+  }
+
+  removeRecChild(cur: any, arr: any[]) {
+    if (Array.isArray(arr)) {
+      arr.forEach((item) => {
+        if (item.parentId === cur.parentId) {
+          item["children"] = [];
+          item["_toggle"] = false;
+        } else {
+          this.removeRecChild(cur, item["children"]);
+        }
+      });
+    }
+  }
+
+  selectExtraCategory(category) {
+    // let name = category.name;
+    // if (category.level == "0") {
+    // } else if (category.level == "1") {
+    //   const parent = this.includeParents(this.allCategoryList, category);
+    //   name = parent;
+    // } else {
+    //   console.log("All Catgories", this.allCategoryList);
+    //   const parent = this.includeParents(this.allCategoryList, category);
+    //   name = parent + " >> " + category.name;
+    // }
+    // debugger;
+    // console.log("Selected Category", name, category._id);
+    // this.categories = [
+    //   { label: name, value: category._id },
+    //   ...this.categories,
+    // ];
+    // this.requestQuotationForm2.patchValue({ productCategory: category._id });
+    // console.log(
+    //   "Selected Category2",
+    //   this.requestQuotationForm2.value.productCategory
+    // );
   }
 
 }
