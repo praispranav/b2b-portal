e6fcf2ae60ea49e8348d166e45c9c0152fa89820
Pranav Kumar <pranav@scorpius.com.my>
2023-01-05 15:59:31 +0000

    env change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://b2buatprod.azurewebsites.net/',
+  apiUrl: 'https://b2buatprod.azurewebsites.net',
   imageStorage: "https://ajayshankar.blob.core.windows.net/b2b/", 
 };
