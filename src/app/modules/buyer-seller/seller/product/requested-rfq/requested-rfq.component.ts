f68dcd699027472af9bdadd6822e0d2830d3c2ad
Pranav Kumar <pranav@scorpius.com.my>
2023-01-19 10:45:42 +0000

    Fixed Bugs and added buyer mqr

--- src/app/modules/buyer-seller/seller/product/requested-rfq/requested-rfq.component.ts
+++ src/app/modules/buyer-seller/seller/product/requested-rfq/requested-rfq.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProviderUserAuthService } from "../../../../../core/providers/auth/provider-user-auth.service";
+import { RequestQuotationService } from "../../../../../core/providers/user/request-quotation.service";
 
 @Component({
   selector: 'app-requested-rfq',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./requested-rfq.component.scss']
 })
 export class RequestedRfqComponent implements OnInit {
+  userInfor: any = null;
+  requestForQutationList: any[] = [];
 
-  constructor() { }
+  constructor(
+    private requestForQuotation: RequestQuotationService,
+    private authService: ProviderUserAuthService
+  ) {}
 
   ngOnInit() {
+    const jwt_decode = this.authService.currentUserValueObjTokenDecoded;
+    console.log("JwtDecode", jwt_decode);
+    if (jwt_decode) {
+      this.userInfor = jwt_decode;
+    }
+
+    this.getRequestForQuotationList();
+  }
+
+  getRequestForQuotationList() {
+    this.requestForQuotation.getRequestForQuotationByUser().subscribe((res) => {
+      if (Array.isArray(res.data)) this.requestForQutationList = res.data;
+    });
   }
 
 }
