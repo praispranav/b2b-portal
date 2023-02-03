83ec7f44a80ae941fb09da56529c35fa72231813
shree42N <riti91557@gmail.com>
2023-02-03 14:46:17 +0530

    added  associate list profile

---
+++ src/app/modules/admin/associate-profile/associate-profile-list/associate-profile-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-associate-profile-list',
+  templateUrl: './associate-profile-list.component.html',
+  styleUrls: ['./associate-profile-list.component.scss']
+})
+export class AssociateProfileListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/modules/admin/associate-profile/associate-profile-view/associate-profile-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-associate-profile-view',
+  templateUrl: './associate-profile-view.component.html',
+  styleUrls: ['./associate-profile-view.component.scss']
+})
+export class AssociateProfileViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
